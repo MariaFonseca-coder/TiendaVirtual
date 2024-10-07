b9c655fe8f9135632794558ff4b65cdf8e374b69
lux-maris <kjimenezm463@ulacit.ed.cr>
2024-10-06 21:21:38 -0600

    Update Administrador.py

--- app/modelos/Administrador.py
+++ app/modelos/Administrador.py
@@ -1,3 +1,6 @@
+from app.modelos import Usuario
+
+
 class Administrador(Usuario):
     def __init__(self, userID, username, password, fechaCreacion=None):
         super().__init__(userID, username, password, "admin", fechaCreacion)
